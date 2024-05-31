780d3f4d416a0b5abf83063223a43bbe4765b907
Abdullahi Shobaloju <sabdullahi@cinnsol.com>
2024-05-31 18:20:04 +0100

    nav initiated

---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+import NavAnnoucement from "./NavAnnouncement"
+
+const Nav = () => {
+    return (
+        <div>
+            This is the nav component
+            <NavAnnoucement />
+        </div>
+    )
+}
+
+export default Nav
---
+++ src/components/NavAnnouncement.tsx
@@ -0,0 +1,11 @@
+
+
+const NavAnnoucement = () => {
+    return(
+        <div>
+            This is the Nav announcement Component
+        </div>
+    )
+}
+
+export default NavAnnoucement
